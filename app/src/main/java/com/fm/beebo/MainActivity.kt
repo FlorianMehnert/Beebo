f480503f4083b0140ff28b8c37630a770c72abef
florian <florian.mehnert@gmail.com>
2025-03-15 11:36:42 +0100

    add some styling

--- app/src/main/java/com/fm/beebo/MainActivity.kt
+++ app/src/main/java/com/fm/beebo/MainActivity.kt
@@ -3,27 +3,75 @@ package com.fm.beebo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.border
+import androidx.compose.material3.MaterialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world!")
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
 
+
+data class Message(val author: String, val body: String)
+
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Text(
+                text = msg.body,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
 }
+
+
 
 
 @Preview
 @Composable
-fun PreviewMessageCard(){
-    MessageCard(name = "Worldd")
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("florian", "Hey, take a look at Jetpack Compose, it's great!")
+    )
 }
