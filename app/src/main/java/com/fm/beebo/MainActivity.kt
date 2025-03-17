7ea088c42013c7cd9b36b906a9865c20274defb8
florian <florian.mehnert@gmail.com>
2025-03-17 23:04:56 +0100

    add adaptable icon

--- app/src/main/java/com/fm/beebo/MainActivity.kt
+++ app/src/main/java/com/fm/beebo/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.fm.beebo.ui.LibrarySearchScreen
+import com.fm.beebo.ui.theme.BeeboTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LibrarySearchScreen()
+            BeeboTheme {
+                LibrarySearchScreen()
+            }
         }
     }
 }
