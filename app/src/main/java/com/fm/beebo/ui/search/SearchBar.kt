6047b62f6afa0f2f90d2e105481a093dcc2f3a0e
florian <florian.mehnert@gmail.com>
2025-03-28 18:25:10 +0100

    add filter icon to search screen

--- app/src/main/java/com/fm/beebo/ui/search/SearchBar.kt
+++ app/src/main/java/com/fm/beebo/ui/search/SearchBar.kt
@@ -1,28 +1,34 @@
 package com.fm.beebo.ui.search
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FilterList
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 
 
@@ -32,7 +38,6 @@ fun SearchBar(
     onQueryChange: (String) -> Unit,
     onSearch: () -> Unit,
     onFilterClick: () -> Unit,
-    showFilter: Boolean
 ) {
     Card(
         modifier = Modifier.fillMaxWidth(),
@@ -43,6 +48,7 @@ fun SearchBar(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
+                .height(60.dp)
         ) {
             OutlinedTextField(
                 value = query,
@@ -51,39 +57,43 @@ fun SearchBar(
                 singleLine = true,
                 leadingIcon = {
                     Icon(
-                        imageVector = Icons.Default.Search, contentDescription = "Suchen"
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Suchen"
                     )
                 },
                 modifier = Modifier.weight(1f)
             )
+
             Spacer(modifier = Modifier.width(8.dp))
 
-            IconButton(onClick = onFilterClick) {
-                Icon(
-                    imageVector = Icons.Default.FilterList,
-                    contentDescription = "Filter",
-                    tint = if (showFilter) MaterialTheme.colorScheme.primary else LocalContentColor.current
-                )
-            }
-            Spacer(modifier = Modifier.width(8.dp))
+            Surface( // Using Surface instead of background() to avoid covering elements
+                shape = RoundedCornerShape(8),
+                modifier = Modifier.height(50.dp).offset(y=4.dp),
+                color = MaterialTheme.colorScheme.primary
 
-            Button(
-                onClick = onSearch,
-                modifier = Modifier
-                    .height(60.dp)
-                    .padding(top = 4.dp)
-                    .offset(y = 2.dp),
-                shape = RoundedCornerShape(4.dp),
             ) {
-                Text("Suchen")
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                ){
+                    IconButton(
+                        onClick = onFilterClick,
+                        modifier = Modifier.clip(RoundedCornerShape(topStart = 4.dp, bottomStart = 4.dp)).width(40.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.FilterList,
+                            contentDescription = "Filter",
+                            tint = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+                    Button(
+                        onClick = onSearch,
+                        modifier = Modifier.clip(RoundedCornerShape(topEnd = 4.dp, bottomEnd = 4.dp)).offset(x = (-10).dp)
+                    ) {
+                        Text("Suchen")
+                    }
+                }
             }
-
-
         }
     }
 
-    if (showFilter) {
-        // Add your filter UI here
-        FilterOptions()
-    }
 }
