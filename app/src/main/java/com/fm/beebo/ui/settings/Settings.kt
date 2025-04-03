33dca5327400f0e7b7ebd1c6a02b9c9b32865aef
florian <florian.mehnert@gmail.com>
2025-04-03 12:20:12 +0200

    bump version

--- app/src/main/java/com/fm/beebo/ui/settings/Settings.kt
+++ app/src/main/java/com/fm/beebo/ui/settings/Settings.kt
@@ -1,27 +1,17 @@
-import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.automirrored.outlined.ArrowBack
-import androidx.compose.material.icons.filled.ArrowOutward
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Link
 import androidx.compose.material.icons.filled.PowerSettingsNew
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
@@ -44,7 +34,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
@@ -140,7 +129,7 @@ fun SettingsScreen(
             ) {
                 MaxPagesSlider(
                     settingsDataStore = settingsDataStore,
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier.fillMaxWidth().padding(top = 8.dp)
                 )
             }
 
@@ -182,16 +171,25 @@ fun SettingsScreen(
 
                     Column(horizontalAlignment = Alignment.Start) {
                         Text(
-                            text = "Version: 1.4",
+                            text = "Version: 1.5",
                             fontSize = 14.sp,
                         )
                         Text(
                             text = "Autor: Merkosh",
                             fontSize = 14.sp,
                         )
+                        Text(
+                            text = "Quellcode",
+                            fontSize = 14.sp,
+                            style = TextStyle(
+                                textDecoration = TextDecoration.Underline
+                            ), modifier = Modifier.clickable {
+                                onShowLibraries()
+                            }
+                        )
                         Spacer(Modifier.height(6.dp))
                         Text(
-                            text = "Open Source Libraries",
+                            text = "Open Source Lizenzen",
                             fontSize = 14.sp,
                             style = TextStyle(
                                 textDecoration = TextDecoration.Underline
