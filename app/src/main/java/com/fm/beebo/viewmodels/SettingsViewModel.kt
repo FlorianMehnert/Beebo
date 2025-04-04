d10ff4a9844b642872ae6a8fca60dda39c3195cc
florian <florian.mehnert@gmail.com>
2025-04-04 11:33:14 +0200

    add more general filter options

--- app/src/main/java/com/fm/beebo/viewmodels/SettingsViewModel.kt
+++ app/src/main/java/com/fm/beebo/viewmodels/SettingsViewModel.kt
@@ -1,6 +1,8 @@
 package com.fm.beebo.viewmodels
 
 import com.fm.beebo.ui.settings.FilterBy
+import com.fm.beebo.ui.settings.FilterOptions
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
@@ -11,10 +13,20 @@ class SettingsViewModel {
     val selectedFilterOption: StateFlow<FilterBy> = _selectedFilterOption
     val appStart: StateFlow<Boolean> = _appStart
 
-    val filterOptions: List<String> = FilterBy.entries.map { it.name }
+    val kindOfMediumList: List<String> = FilterBy.entries.map { it.name }
+    val selectedYear: Flow<Int> = MutableStateFlow(2025)
+    val sortBy: Flow<Pair<FilterOptions, Boolean>> = MutableStateFlow(Pair(FilterOptions.YEAR, true))
 
     fun setSelectedFilterOption(option: Int) {
         _selectedFilterOption.value = FilterBy.entries.toTypedArray()[option]
+    }
+
+    fun setSelectedYear(year: Int) {
+        (selectedYear as MutableStateFlow<Int>).value = year
+    }
+
+    fun setSortBy(option: FilterOptions, value: Boolean){
+        (sortBy as MutableStateFlow<Pair<FilterOptions, Boolean>>).value = Pair(option, value)
     }
 
     fun setAppStart(option: Boolean) {
