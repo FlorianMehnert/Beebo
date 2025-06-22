45b8414b88d797ad56789d5236074e2ecdd1415c
florian <florian.mehnert@gmail.com>
2025-06-22 20:12:00 +0200

    remove imports

--- app/src/main/java/com/fm/beebo/ui/search/SearchStatus.kt
+++ app/src/main/java/com/fm/beebo/ui/search/SearchStatus.kt
@@ -37,8 +37,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 
 @Composable
