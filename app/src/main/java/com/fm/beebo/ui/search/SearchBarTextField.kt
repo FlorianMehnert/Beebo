0ba6a9c19735f4512a066d9047a2dc987e988e9d
florian <florian.mehnert@gmail.com>
2025-06-22 20:12:00 +0200

    remove imports

--- app/src/main/java/com/fm/beebo/ui/search/SearchBarTextField.kt
+++ app/src/main/java/com/fm/beebo/ui/search/SearchBarTextField.kt
@@ -1,18 +1,11 @@
 package com.fm.beebo.ui.search
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
